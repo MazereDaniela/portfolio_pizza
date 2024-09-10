09af13e62c78481bf1d55195fe244bf4e3eba540
Даниела Мазэре <danielamazere@Air-Daniela.lan>
2024-09-10 23:35:42 +0300

    pizza

---
+++ js/slick.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('.main-slider_2').slick({
+        arrows:false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+      });
+  });
+
+  $(document).ready(function(){
+    $('.main-slider_3').slick({
+        arrows:false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+      });
+  });
+
+  $(document).ready(function(){
+    $('.main-slider_4').slick({
+        dots: true,
+        slidesToShow: 1,
+    });
+});
